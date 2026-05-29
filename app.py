2f498b24807c38e17e64a80c9d986ff09fb06220
Student <kirilkirdan@gmail.com>
2026-05-29 05:26:14 +0000

    Change message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(status="ok", message="Hello from Flask CI/CD!")
+    return jsonify(status="ok", message="Hello from Flask CI/CD! Updated by Jenkins")
 
 @app.route("/health")
 def health():
