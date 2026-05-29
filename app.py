14ff0946007aaa2aec36748c38d0f3a4d0c0d5a0
Student <kirilkirdan@gmail.com>
2026-05-29 01:25:17 +0000

    Initial Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify(status="ok", message="Hello from Flask CI/CD!")
+
+@app.route("/health")
+def health():
+    return jsonify(status="healthy")
